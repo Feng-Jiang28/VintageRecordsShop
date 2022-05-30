ec2713bb204a8bd209c00b34439807950e8a9f7e
Feng-Jiang28 <jiangfeng19980130@gmail.com>
2022-05-30 18:24:13 +0800

    added post and getbyid methods

--- main.go
+++ main.go
@@ -27,11 +27,11 @@ var albums = []album{
 
 func main() {
 	router := gin.Default()
+    router.GET("/albums", getAlbums)
+    router.GET("/albums/:id", getAlbumByID)
+    router.POST("/albums", postAlbums)
 
-	//To associate the GET http method and /albums with a handler function
-	router.GET("/albums", getAlbums) // passing the function
-
-	router.Run("localhost:8080")
+    router.Run("localhost:8080")
 }
 
 // gin.Context request details, validates and serializes JSON,
@@ -43,3 +43,42 @@ func getAlbums(c *gin.Context) {
 // go get .
 
 // add dependency for your module. . means get dependencies for code in the current directory.
+//go run . to run the code in current directoty
+
+// Write a handler to add a new item
+
+// postAlbums adds an album from JSON received in the request body.
+func postAlbums(c *gin.Context) {
+    var newAlbum album
+
+    // Call BindJSON to bind the received JSON to
+    // newAlbum.
+    if err := c.BindJSON(&newAlbum); err != nil {
+        return
+    }
+
+    // Add the new album to the slice.
+    albums = append(albums, newAlbum)
+    c.IndentedJSON(http.StatusCreated, newAlbum)
+}
+
+
+// Write a handler to return a specific item.
+
+// GET/albums/[id], you want to return the album whose id
+
+// getAlbumByID locates the album whose ID values matches the id 
+// parameter sent by the client, then returns that album as a response
+func getAlbumByID(c *gin.Context) {
+    id := c.Param("id")
+
+    // Loop through the list of albums, looking for
+    // an album whose ID value matches the parameter.
+    for _, a := range albums {
+        if a.ID == id {
+            c.IndentedJSON(http.StatusOK, a)
+            return
+        }
+    }
+    c.IndentedJSON(http.StatusNotFound, gin.H{"message": "album not found"})
+}
